77f250db096c90daa4e4ae9d3cf5027a9aa7bf73
Xavier Schildwachter <x.schildwachter@sagebase.org>
2018-04-09 13:59:29 -0700

    Add peering to SynapseDev vpc

--- update_cf_stack.sh
+++ update_cf_stack.sh
@@ -242,3 +242,28 @@ elif [[ $error_code -ne 0 ]]; then
 else
   echo $message
 fi
+
+STACK_NAME="peering-synapsedev"
+CF_TEMPLATE="VPCPeer.yml"
+echo -e "\nDeploying CF template cf_templates/$CF_TEMPLATE"
+UPDATE_CMD="aws cloudformation update-stack \
+--stack-name $STACK_NAME \
+--capabilities CAPABILITY_NAMED_IAM \
+--notification-arns $CloudformationNotifyLambdaTopicArn \
+--template-body file://cf_templates/$CF_TEMPLATE \
+--parameters \
+ParameterKey=PeerVPC,ParameterValue="vpc-1362e468" \
+ParameterKey=PeerVPCOwner,ParameterValue=\"$SynapseDevAwsAccountId\" \
+ParameterKey=PeerVPCCIDR,ParameterValue="10.11.0.0/16" \
+ParameterKey=PeerRoleName,ParameterValue="essentials-VPCPeeringAuthorizerRole-ANWU0AZR9EZ7""
+message=$($UPDATE_CMD 2>&1 1>/dev/null)
+error_code=$(echo $?)
+if [[ $error_code -ne 0 && $message =~ .*"No updates are to be performed".* ]]; then
+  echo -e "\nNo stack changes detected. An update is not required."
+  error_code=0
+elif [[ $error_code -ne 0 ]]; then
+  echo $message
+  exit $error_code
+else
+  echo $message
+fi
